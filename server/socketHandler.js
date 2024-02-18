b4b12ab9dc84aa6a4df9f41bd1827f11ed629659
Yousef Saeed <yousafesaeed@gmail.com>
2024-02-18 19:55:11 +0200

    [feat] Save & retrieve messages

--- server/socketHandler.js
+++ server/socketHandler.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const User = require('./models/user.model');
+const Message = require("./models/message.model");
 
 module.exports = (io) => {
   // Authentication middleware
@@ -26,17 +27,29 @@ module.exports = (io) => {
     const socketRoomMap = new Map();
 
     // Handle 'join' event when a client joins the chat room
-    socket.on('join', (room) => {
+    socket.on('join', async (room) => {
       socket.emit('username', socket.username);
       socket.join(room);
       socketRoomMap.set(socket.username, room);
       socket.emit('joined', `You joined ${room}`);
+
+      const messages = await Message.find({ roomId: room }).sort({ createdAt: 1 });
+      messages.forEach((message) => {
+        socket.emit('chat message', `${message.username}: ${message.message}`);
+      });
+
       socket.broadcast.to(room).emit('user joined', `${socket.username} joined ${room}`);
     });
 
     // Handle 'chat message' event when a client sends a message
-    socket.on('chat message', (room, message) => {
-      socket.broadcast.to(room).emit('chat message', `${socket.username}: ${message}`);
+    socket.on('chat message', async (room, message) => {
+      const newMessage = new Message({
+        roomId: room,
+        username: socket.username,
+        message,
+      });
+      await newMessage.save();
+      io.to(room).emit('chat message', `${socket.username}: ${message}`);
     });
 
     // Handle 'disconnect' event when a client disconnects
