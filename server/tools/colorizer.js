cb8f66f0d0fcaeda4214671a94b90f35c47b30e9
Yousef Saeed <yousafesaeed@gmail.com>
2024-02-21 22:25:47 +0200

    [refactor] Refine escape codes list

--- server/tools/colorizer.js
+++ server/tools/colorizer.js
@@ -1,6 +1,5 @@
-const escapeCodes = {
+const styleCodes = {
   reset: "\x1b[0m",
-  // Styles
   bold: "\x1b[1m",
   dim: "\x1b[2m",
   italic: "\x1b[3m",
@@ -8,47 +7,51 @@ const escapeCodes = {
   inverse: "\x1b[7m",
   hidden: "\x1b[8m",
   strikethrough: "\x1b[9m",
-  // Foreground colors
-  fgBlack: "\x1b[30m",
-  fgRed: "\x1b[31m",
-  fgGreen: "\x1b[32m",
-  fgYellow: "\x1b[33m",
-  fgBlue: "\x1b[34m",
-  fgMagenta: "\x1b[35m",
-  fgCyan: "\x1b[36m",
-  fgWhite: "\x1b[37m",
-  fgBrightBlack: "\x1b[90m",
-  fgBrightRed: "\x1b[91m",
-  fgBrightGreen: "\x1b[92m",
-  fgBrightYellow: "\x1b[93m",
-  fgBrightBlue: "\x1b[94m",
-  fgBrightMagenta: "\x1b[95m",
-  fgBrightCyan: "\x1b[96m",
-  fgBrightWhite: "\x1b[97m",
-  // Background colors
-  bgBlack: "\x1b[40m",
-  bgRed: "\x1b[41m",
-  bgGreen: "\x1b[42m",
-  bgYellow: "\x1b[43m",
-  bgBlue: "\x1b[44m",
-  bgMagenta: "\x1b[45m",
-  bgCyan: "\x1b[46m",
-  bgWhite: "\x1b[47m",
-  bgBrightBlack: "\x1b[100m",
-  bgBrightRed: "\x1b[101m",
-  bgBrightGreen: "\x1b[102m",
-  bgBrightYellow: "\x1b[103m",
-  bgBrightBlue: "\x1b[104m",
-  bgBrightMagenta: "\x1b[105m",
-  bgBrightCyan: "\x1b[106m",
-  bgBrightWhite: "\x1b[107m",
+};
+
+const fgColorCodes = {
+  black: "\x1b[30m",
+  red: "\x1b[31m",
+  green: "\x1b[32m",
+  yellow: "\x1b[33m",
+  blue: "\x1b[34m",
+  magenta: "\x1b[35m",
+  cyan: "\x1b[36m",
+  white: "\x1b[37m",
+  brightBlack: "\x1b[90m",
+  brightRed: "\x1b[91m",
+  brightGreen: "\x1b[92m",
+  brightYellow: "\x1b[93m",
+  brightBlue: "\x1b[94m",
+  brightMagenta: "\x1b[95m",
+  brightCyan: "\x1b[96m",
+  brightWhite: "\x1b[97m",
+};
+
+const bgColorCodes = {
+  black: "\x1b[40m",
+  red: "\x1b[41m",
+  green: "\x1b[42m",
+  yellow: "\x1b[43m",
+  blue: "\x1b[44m",
+  magenta: "\x1b[45m",
+  cyan: "\x1b[46m",
+  white: "\x1b[47m",
+  brightBlack: "\x1b[100m",
+  brightRed: "\x1b[101m",
+  brightGreen: "\x1b[102m",
+  brightYellow: "\x1b[103m",
+  brightBlue: "\x1b[104m",
+  brightMagenta: "\x1b[105m",
+  brightCyan: "\x1b[106m",
+  brightWhite: "\x1b[107m",
 };
 
 function colorize(string, fgColor, bgColor, style) {
-  const fgColorCode = escapeCodes[fgColor] || "";
-  const bgColorCode = escapeCodes[bgColor] || "";
-  const styleCode = escapeCodes[style] || "";
-  return `${styleCode}${fgColorCode}${bgColorCode}${string}${escapeCodes.reset}`;
+  const fgColorCode = fgColorCodes[fgColor] || "";
+  const bgColorCode = bgColorCodes[bgColor] || "";
+  const styleCode = styleCodes[style] || "";
+  return `${styleCode}${fgColorCode}${bgColorCode}${string}${styleCodes.reset}`;
 }
 
 module.exports = colorize;
